ae515f8d247320d108435b4fc005fd070a0dbd47
Vinicius Monteiro <vinicius.monteiro@askblue.com>
2024-12-16 14:41:14 +0000

    feat: Adding checkout scenarios

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -1,12 +1,13 @@
 import test from '../pages/basePage'
 import { dataProduct } from '../data/dataconfig'
 
+// This test validates that a specific product can be searched and found on the homepage.
 test('Scenario: Search for a specific product', async ({ homepage, productpage }) => {
     const product = dataProduct.specificProdut
     await test.step('Given the user accesses the homepage', async () => {
         await homepage.accessHomePage()
     })
-    await test.step('When they search for a specific item', async () => {
+    await test.step('When user search for a specific item', async () => {
         await productpage.searchProduct(product)
     })
     await test.step('Then it validates that the searched item is returned', async () => {
@@ -14,29 +15,49 @@ test('Scenario: Search for a specific product', async ({ homepage, productpage }
     })
 })
 
+// This test verifies that a logged-in user can search for a product, add it to the cart, and proceed to the checkout.
 test('Scenario: Checkout with user logged in', async ({ loginpage, homepage, productpage }) => {
     const product = dataProduct.specificProdut
-    await test.step('Given the user accesses the homepage', async () => {
+    const productCart = dataProduct.specificProdutCart
+    await test.step('Given the user accesses the homepage logged in', async () => {
         await homepage.accessHomePage()
+        await homepage.accessLoginPage()
+        await loginpage.doLogin()
+        await loginpage.validateLogin()
     })
-    await test.step('When they search for a specific item', async () => {
+    await test.step('When user search for a specific item', async () => {
         await productpage.searchProduct(product)
     })
-    await test.step('Then it validates that the searched item is returned', async () => {
-        await productpage.validateProductSearched(product)
+    await test.step('And adds the product to the cart', async () => {
+        await productpage.addProductCart()
+        await productpage.validateProductAddedCart(productCart)
+    })
+    await test.step('And go to checkout', async () => {
+        await productpage.checkoutMiniCart()
+    })
+    await test.step('Then validate that the user is on the shipping page', async () => {
+        await productpage.validateCheckoutLoggedIn()
     })
 })
 
+// This test ensures that a user logged out is redirected to the login page when attempting to proceed to checkout.
 test('Scenario: Checkout with user logged out', async ({ loginpage, homepage, productpage }) => {
     const product = dataProduct.specificProdut
+    const productCart = dataProduct.specificProdutCart
     await test.step('Given the user accesses the homepage', async () => {
         await homepage.accessHomePage()
     })
-    await test.step('When they search for a specific item', async () => {
+    await test.step('When user search for a specific item', async () => {
         await productpage.searchProduct(product)
     })
-    await test.step('Then it validates that the searched item is returned', async () => {
-        await productpage.validateProductSearched(product)
+    await test.step('And adds the product to the cart', async () => {
+        await productpage.addProductCart()
+        await productpage.validateProductAddedCart(productCart)
+    })
+    await test.step('And go to checkout', async () => {
+        await productpage.checkoutMiniCart()
+    })
+    await test.step('Then validate that the user is on the login page', async () => {
+        await loginpage.validateUserLogout()
     })
 })
-
